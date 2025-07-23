6199b791a3d88d5c0fb046080c27906d8c2c776f
Khan Vuthy <khanvuthy@Khans-MacBook-Pro.local>
2025-07-23 21:15:31 +0700

    [ADD] angkot-attendance: add new fields: check_in_note, check_out_note, in_mode/out_mode (mobile)

--- angkot-attendance/models/hr_attendance.py
+++ angkot-attendance/models/hr_attendance.py
@@ -9,3 +9,10 @@ class HrAttendance(models.Model):
     check_out_lat = fields.Char()
     check_out_lng = fields.Char()
     radius = fields.Char()
+
+    check_in_note = fields.Char()
+    check_out_note = fields.Char()
+    in_mode = fields.Selection(
+        selection_add=[('mobile', 'Mobile')]
+    )
+    out_mode = fields.Selection(selection_add=[('mobile', 'Mobile')])
